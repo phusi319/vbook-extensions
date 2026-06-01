8fe15bc9e4af80d2534f6f40b2c5d7e288e242d5
phusi319 <phusi.319@gmail.com>
2026-06-02 02:03:18 +0300

    Fix mangadot plugin.json formatting

---
+++ test_mangadot_zip/src/chap.js
@@ -0,0 +1,18 @@
+function execute(url) {
+    var doc = Http.get(url).html();
+    var el = doc.select('img.object-cover'); // Depending on chapter HTML structure
+    var data = [];
+    
+    // We might need to inspect chapter.html to be sure
+    // Let's grab all images that have data-src or src and look like manga pages
+    var imgs = doc.select('img');
+    for (var i = 0; i < imgs.size(); i++) {
+        var e = imgs.get(i);
+        var src = e.attr('data-src') || e.attr('src');
+        if (src && (src.indexOf('uploads') !== -1 || src.indexOf('mangadex') !== -1)) {
+            data.push(src);
+        }
+    }
+    
+    return Response.success(data);
+}
