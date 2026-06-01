2a81f4c687dadb58f4825faa8d158f2ce9c51003
phusi319 <phusi.319@gmail.com>
2026-06-01 23:55:20 +0300

    fix: chap.js return JSONArray instead of HTML string

--- foxtruyen/src/chap.js
+++ foxtruyen/src/chap.js
@@ -4,40 +4,27 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
+        let data = [];
 
-        let images = [];
-        doc.select(".content_detail_manga img, .content_detail img").forEach(function(e) {
+        // Comic/Manga: images inside .content_detail_manga or .content_detail
+        let imgs = doc.select(".content_detail_manga img, .content_detail img");
+        for (let i = 0; i < imgs.size(); i++) {
+            let e = imgs.get(i);
             let src = e.attr("src");
             if (!src || src.length === 0) {
                 src = e.attr("data-src");
             }
-            if (src && src.length > 0
-                && src.indexOf("loading.jpg") === -1
-                && src.indexOf("pixel") === -1
-                && src.indexOf("1x1") === -1
-                && src.indexOf("banner") === -1
-                && src.indexOf("data-cl-spot") === -1) {
-                if (src.indexOf("hinhgg.com") !== -1) {
-                    images.push(src);
-                }
+            // Only include actual manga images (from hinhgg.com CDN)
+            if (src && src.length > 0 && src.indexOf("hinhgg.com") > -1) {
+                data.push({
+                    link: src,
+                    fallback: []
+                });
             }
-        });
-
-        if (images.length > 0) {
-            let content = "";
-            images.forEach(function(img) {
-                content += '<img src="' + img + '">\n';
-            });
-            return Response.success(content);
         }
 
-        let article = doc.select(".content_detail, .box_content, article").first();
-        if (article) {
-            let txt = article.html();
-            txt = txt.replace(/<script[^>]*>[\s\S]*?<\/script>/gi, "");
-            txt = txt.replace(/<ins[^>]*>[\s\S]*?<\/ins>/gi, "");
-            txt = txt.replace(/<div[^>]*data-cl-spot[^>]*>[\s\S]*?<\/div>/gi, "");
-            return Response.success(txt);
+        if (data.length > 0) {
+            return Response.success(data);
         }
 
         return null;
