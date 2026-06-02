f554a7f65f7d05dfe170362f0498e76e15bdb31e
phusi319 <phusi.319@gmail.com>
2026-06-02 09:00:36 +0300

    Mangadot v19: Remove non-standard episodes field from detail.js to fix Add Bookshelf crash

---
+++ make_zip.py
@@ -0,0 +1,16 @@
+import zipfile
+import os
+import sys
+
+def make_plugin_zip(ext_dir, output_zip):
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zf:
+        zf.write(os.path.join(ext_dir, 'plugin.json'), 'plugin.json')
+        icon = os.path.join(ext_dir, 'icon.png')
+        if os.path.exists(icon):
+            zf.write(icon, 'icon.png')
+        src_dir = os.path.join(ext_dir, 'src')
+        for f in sorted(os.listdir(src_dir)):
+            zf.write(os.path.join(src_dir, f), 'src/' + f)
+
+if __name__ == '__main__':
+    make_plugin_zip(sys.argv[1], sys.argv[2])
--- mangadot/src/detail.js
+++ mangadot/src/detail.js
@@ -19,7 +19,6 @@ function execute(url) {
     var description = '';
     var ongoing = true;
     var genres = [];
-    var episodes = [];
 
     // Try .data turbo-stream for metadata
     var dataUrl = requestUrl + '.data';
@@ -100,75 +99,6 @@ function execute(url) {
     if (!name) name = "Unknown"; // prevent Add Bookshelf crash at all costs
     if (!cover) cover = "";
 
-    // Extract manga ID from URL: /manga/{id}
-    var mangaId = '';
-    var idMatch = requestUrl.match(/\/manga\/(\d+)/);
-    if (idMatch) {
-        mangaId = idMatch[1];
-    }
-
-    // Fetch chapters from API
-    if (mangaId) {
-        try {
-            var apiUrl = BASE_URL + '/api/manga/' + mangaId + '/chapters/list';
-            var chapList = null;
-            try {
-                var str = Http.get(apiUrl).string();
-                if (str && (str.indexOf('[{') === 0 || str.indexOf('[]') === 0)) {
-                    chapList = JSON.parse(str);
-                }
-            } catch (e) {}
-            
-            if (!chapList) {
-                var chapResp = fetch(apiUrl);
-                if (chapResp.ok) chapList = chapResp.json();
-            }
-
-            if (chapList && Array.isArray(chapList)) {
-                var sourceMap = {};
-
-                    for (var i = 0; i < chapList.length; i++) {
-                        var ch = chapList[i];
-                        var chapNum = ch.chapter_number;
-                        var chapTitle = ch.chapter_title || '';
-                        var chapId = ch.id;
-                        var groupName = ch.group_name || ch.scanlator_name || 'Default';
-                        if (!groupName || groupName === 'null') groupName = 'Default';
-
-                        // Build chapter display name
-                        var chapName = 'Ch. ' + chapNum;
-                        if (chapTitle && chapTitle.indexOf('Chapter') !== 0 && chapTitle.indexOf('Episode') !== 0) {
-                            chapName = chapName + ' - ' + chapTitle;
-                        }
-
-                        if (!sourceMap[groupName]) {
-                            sourceMap[groupName] = [];
-                        }
-
-                        var sourceParam = ch.source ? '?source=' + ch.source : '';
-
-                        sourceMap[groupName].push({
-                            name: chapName,
-                            url: BASE_URL + '/chapter/' + chapId + sourceParam,
-                            host: BASE_URL
-                        });
-                    }
-
-                    // Convert to episodes (tabs by source group)
-                    for (var group in sourceMap) {
-                        if (sourceMap.hasOwnProperty(group)) {
-                            episodes.push({
-                                title: group,
-                                urls: sourceMap[group]
-                            });
-                        }
-                }
-            }
-        } catch (e) {
-            // Chapter API failed
-        }
-    }
-
     return Response.success({
         name: name,
         cover: cover,
@@ -176,7 +106,6 @@ function execute(url) {
         description: description,
         host: BASE_URL,
         ongoing: ongoing,
-        genres: genres,
-        episodes: episodes
+        genres: genres
     });
 }
