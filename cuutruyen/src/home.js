80e1da0ea42ef70cfc1914e92bab337baf90dd1c
phusi319 <phusi.319@gmail.com>
2026-06-04 05:25:09 +0300

    add Truyen Full tab v8

--- cuutruyen/src/home.js
+++ cuutruyen/src/home.js
@@ -5,6 +5,7 @@ function execute() {
         {title: "Mới cập nhật", input: API_URL + "/mangas/recently_updated", script: "gen.js"},
         {title: "Nổi bật tuần", input: API_URL + "/mangas/top?duration=week", script: "gen.js"},
         {title: "Nổi bật tháng", input: API_URL + "/mangas/top?duration=month", script: "gen.js"},
-        {title: "Nổi bật mọi thời đại", input: API_URL + "/mangas/top?duration=all", script: "gen.js"}
+        {title: "Nổi bật mọi thời đại", input: API_URL + "/mangas/top?duration=all", script: "gen.js"},
+        {title: "Truyện Full", input: API_URL + "/mangas/recently_updated?status=completed", script: "gen.js"}
     ]);
 }
