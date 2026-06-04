a19a52c33a196441084930b557723e81d2a58818
phusi319 <phusi.319@gmail.com>
2026-06-04 04:06:45 +0300

    Add .gitignore and remove temp files

--- test_links.js
+++
@@ -1,6 +0,0 @@
-fetch('https://truyenqqko.com').then(r => r.text()).then(html => {
-    const matches = html.match(/href="([^"]*truyen-tranh[^"]*)"/g);
-    if (matches) {
-        matches.slice(0, 10).forEach(m => console.log(m));
-    }
-});
