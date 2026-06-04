8c789ea0cb74f804756fb718f9094280ba289d7a
phusi319 <phusi.319@gmail.com>
2026-06-04 05:46:43 +0300

    add full info detail v11

--- cuutruyen/src/detail.js
+++ cuutruyen/src/detail.js
@@ -10,14 +10,36 @@ function execute(url) {
     if (json && json.data) {
         var data = json.data;
 
+        var author = data.author ? data.author.name : "Đang cập nhật";
+        var team = data.team ? data.team.name : "";
+        var titles = (data.titles && data.titles.length) ? data.titles.map(function(t) { return t.title; }).join(", ") : "";
+        var genres = (data.tags && data.tags.length) ? data.tags.map(function(t) { return t.name; }).join(", ") : "";
+        var isCompleted = false;
+        if (data.tags) {
+            for (var i = 0; i < data.tags.length; i++) {
+                if (data.tags[i].slug === 'da-hoan-thanh') {
+                    isCompleted = true;
+                    break;
+                }
+            }
+        }
+
+        var detail = "Tác giả: " + author;
+        if (team) detail += "<br>Nhóm dịch: " + team;
+        if (titles) detail += "<br>Tên khác: " + titles;
+        if (genres) detail += "<br>Thể loại: " + genres;
+        detail += "<br>" + (data.chapters_count || 0) + " Chương";
+
+        var description = data.full_description || data.description || "";
+
         return Response.success({
             name: data.name,
             cover: data.cover_url,
-            author: data.author ? data.author.name : "Đang cập nhật",
-            description: data.description || "",
-            detail: (data.author ? data.author.name : "") + "<br>" + (data.chapters_count || 0) + " Chương",
+            author: author,
+            description: description,
+            detail: detail,
             host: BASE_URL,
-            ongoing: data.status !== "completed"
+            ongoing: !isCompleted
         });
     }
 
